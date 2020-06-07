fb567d1241966201e41282735eb03d32376ad637
rodrigocordulab <rodrigo.cordula@gmail.com>
2020-06-06 21:43:14 -0300

    Aplicando controle de perfil de usuários

--- pages/registraChamado/registra_chamado.php
+++ pages/registraChamado/registra_chamado.php
@@ -1,11 +1,13 @@
 <?php
+    session_start();
+
 
     $titulo = str_replace('#', '-', $_POST['titulo']);
     $categoria = str_replace('#', '-', $_POST['categoria']);
     $descricao = str_replace('#', '-', $_POST['descricao']);
 
 
-    $text = $titulo  . '#' . $categoria . '#' . $descricao . PHP_EOL;
+    $text = $_SESSION['id']. '#' . $titulo  . '#' . $categoria . '#' . $descricao . PHP_EOL;
 
     $arquivo = fopen('arquivo.txt','a');
 
--- services/valida_login.php
+++ services/valida_login.php
@@ -7,16 +7,34 @@
     //variavel que verifica se a autenticacao foi realizada
 
     $usuario_autenticado = false;
+    $usuario_id = null;
+    $usuario_perfil_id = null;
+
+    $perfis = array(1 => 'Administrativo', 2 => 'Usuário' );
 
 
     //usuarios do sistema
 
     $usuarios_app = array(
       array(
-        'email' => 'adm@teste.com.br', 'senha' => '1234'
+        'id' => 1,
+        'email' => 'adm@teste.com.br', 'senha' => '1234',
+        'perfil_id' => 1
       ),
       array(
-        'email' => 'user@teste.com.br', 'senha' => 'abcd'
+        'id' => 2,
+        'email' => 'user@teste.com.br', 'senha' => '1234',
+        'perfil_id' => 1
+      ),
+      array(
+        'id' => 3,
+        'email' => 'jose@teste.com.br', 'senha' => '1234',
+        'perfil_id' => 2
+      ),
+      array(
+        'id' => 4,
+        'email' => 'maria@teste.com.br', 'senha' => '1234',
+        'perfil_id' => 2
       ),
     );
 
@@ -28,12 +46,16 @@
 
         if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
           $usuario_autenticado = true;
+          $usuario_id = $user['id'];
+          $usuario_perfil_id = $user['perfil_id'];
         }
       }
 
         if($usuario_autenticado){
           echo 'Usuário autenticado';
           $_SESSION['autenticado'] = 'SIM';
+          $_SESSION['id'] = $usuario_id;
+          $_SESSION['perfil_id'] = $usuario_perfil_id;
           header('Location: ../pages/home/home.php');
         } else {
           $_SESSION['autenticado'] = 'NAO';
